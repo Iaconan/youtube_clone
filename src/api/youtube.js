484c786fcf2174bcdb823fb6f3b10a37b3dcc1e1
Vincenzo Iacona <vincenzoiacona@hotmail.com>
2022-04-17 23:53:29 +0200

    searchbar , video , video list added and youtube clone completed but not styled

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://www.googlepapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: 'AIzaSyD7hFueTd4vlJkE6Glhu9xU9ylFAeC-XkE'
-    }
-})
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    
+});
